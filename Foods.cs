50fc1e1c90a6f4b3daa66b9bb090420b46edf995
Mikaeldwsn <mikaels2005@gmail.com>
2024-06-13 13:11:49 +0700

    Add project files.

---
+++ Foods.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Project_OOP_Trio_Rawr
+{
+    public class Foods : Items
+    {
+    }
+}
---
+++ Items.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace Project_OOP_Trio_Rawr
+{
+    public class Items
+    {
+        private string name;
+        private Image picture;
+        private int price;
+        
+        protected string DisplayItems()
+        {
+            return "";
+        }
+
+        public string Display()
+        {
+            return "";
+        }
+    }
+}
